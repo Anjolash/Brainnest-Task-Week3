208621cd70d905042db364f3824ed034f6ffaca5
what <whatagoal021@gmail.com>
2022-09-10 00:05:45 +0100

    update week4

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const scoresPara = document.querySelector(".displayResults");
 const winGifVisibility = document.querySelector(".wingif");
 const loseGifVisibility = document.querySelector(".losegif");
 const tieGifVisibility = document.querySelector(".tiegif");
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorsBtn = document.querySelector(".scissors");
+const buttons = document.querySelectorAll(".playerOptions button")
 winGifVisibility.classList += " notVisible";
 loseGifVisibility.classList += " notVisible";
 tieGifVisibility.classList += " notVisible";
@@ -13,24 +17,30 @@ tieGifVisibility.classList += " notVisible";
 function win(){
     userScore__span.innerHTML = playerScore;
     computerScore__span.innerHTML = computerScore;
-    if(playerScore > computerScore){
-        scoresPara.innerHTML = "YOU WIN!!!!";
-        winGifVisibility.classList.remove("notVisible")
-    }
-    else if(playerScore < computerScore){
-        scoresPara.innerHTML = "YOU LOSE!!!";
-        loseGifVisibility.classList.remove("notVisible")
-    }
-    else{
-        scoresPara.innerHTML = "DRAW";
-        tieGifVisibility.classList.remove("notVisible")
-    }
+    if(playerScore == "5" || computerScore == "5"){
+        if(playerScore > computerScore){
+            scoresPara.innerHTML = "GAME OVER YOU WIN!!!! <br> Select option to play again";
+            winGifVisibility.classList.remove("notVisible")
+        }
+        else if(playerScore < computerScore){
+            scoresPara.innerHTML = "GAME OVER YOU LOSE!!! <br> Select option to play again";
+            loseGifVisibility.classList.remove("notVisible")
+        }
+        else{
+            scoresPara.innerHTML = "DRAW";
+            tieGifVisibility.classList.remove("notVisible")
+        }
+    }    
 }
+
+
+
+
 
 
 function computerPlay(play){
     var pick = ["rock", "paper", "scissors"]
-    play = pick[Math.round(Math.random()*(pick.length-1))]
+    play = pick[Math.round(Math.random()*(pick.length -1))]
     return play
 }
 
@@ -39,53 +49,60 @@ function playRound(playerSelection, computerSelection ){
     if(playerSelection === computerSelection ){
         return "Draw"
     }
-    if(playerSelection == "rock" && computerSelection === "scissors" ){
+    else if(
+        playerSelection == "rock" && computerSelection === "scissors"  || 
+        playerSelection == "paper" && computerSelection === "rock" ||
+        playerSelection == "scissors" && computerSelection === "paper" 
+    ){
         playerScore ++ ;
-        return "You win! Rock beats scissors"    
+        return "You win! " + `${playerSelection}` +  " beats " + `${computerSelection}`
     }
-    else if(playerSelection == "rock" && computerSelection === "paper" ){
+    else if(
+        playerSelection == "rock" && computerSelection === "paper" ||
+        playerSelection == "paper" && computerSelection === "scissors" ||
+        playerSelection == "scissors" && computerSelection === "rock" 
+        ){
         computerScore ++
-        return "You lose! paper beats rock"
+        return "You lose! " + `${computerSelection}` + " beats " + `${playerSelection}`
     }
-    else if(playerSelection == "paper" && computerSelection === "scissors" ){
-        computerScore ++
-        return "You lose! scissors beats Paper"
-    }
-    else if(playerSelection == "paper" && computerSelection === "rock" ){
-        playerScore ++
-        return "You Win! paper beats rock"
-    }
-    else if(playerSelection == "scissors" && computerSelection === "rock" ){
-        computerScore ++
-        return "You Lose! rock beats scissors"
-    }
-    else if(playerSelection == "scissors" && computerSelection === "paper" ){
-        playerScore ++
-        return "You Win! scissors beats paper"
-    } 
+}
+
+function reset(){
+    playerScore = 0;
+    computerScore = 0;
+    winGifVisibility.classList += " notVisible";
+    loseGifVisibility.classList += " notVisible";
+    tieGifVisibility.classList += " notVisible";
 }
 
 function game(){
-    var playTurn = "play";
+    //var playTurn = "play here";
     var playerSelection = "";
-    for(let i = 0; i < 5; i++){
-        playerSelection = prompt(playTurn)
-        playerSelection = playerSelection.toLowerCase();
-        
-        computerPlay()
-        const computerSelection = computerPlay();
-        console.log(playerSelection)
-        playRound(playerSelection)
-        console.log(computerSelection)
-        console.log(playRound(playerSelection, computerSelection))
-        console.log(playerScore)
-        console.log(computerScore)
-    }
+    buttons.forEach(btn =>{
+        btn.addEventListener('click',(e)=>{
+            if(playerScore == 5 || computerScore == 5){
+                reset()
+            }
+            let playerSelection = e.target.className;
+            
+            playerSelection = playerSelection.toLowerCase();
+            
+            computerPlay()
+            const computerSelection = computerPlay();
+            console.log(playerSelection)
+            playRound(playerSelection)
+            console.log(computerSelection)
+            console.log(playRound(playerSelection, computerSelection))
+            console.log(playerScore)
+            console.log(computerScore)
+            win()
+        })
+    })
 }
 
 
 game()
-win()
+
 
 
 
